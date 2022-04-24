4dd1f2eda1409f60d5fbad17d76cfb24f7028181
Caleb Kussmaul <caleb.kussmaul@whoop.com>
2022-04-24 18:23:18 -0400

    Migrate to Python 3

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import stranger
 
 import connexion
-from gevent.wsgi import WSGIServer
+from gevent.pywsgi import WSGIServer
 
 if __name__ == '__main__':
     stranger.start_client()
 
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.add_api('swagger.yaml', arguments={'title': 'Stranger board'})
-    http_server = WSGIServer(('', 80), app)
+    http_server = WSGIServer(('', 8080), app)
     http_server.serve_forever()
